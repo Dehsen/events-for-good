6fba5fbf03ed931bdf976f3ca4ed6860a0d3cce9
Dehsen Ragavulu <lccdehsen@gmail.com>
2021-08-26 14:57:36 +0200

    built small app with basic functionality

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="nav">
+    <router-link :to="{name: 'EventList'}">Events</router-link> |
+    <router-link :to="{name: 'About'}">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  // background-color: #61656a;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+h4 {
+  font-weight: 20px;
+}
+</style>
